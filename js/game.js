e0447c7f2e528ae45572294db2acdd07682157fc
Brady Murren <bmurren5@gmail.com>
2016-02-17 03:22:21 -0500

    initial commit to GitHub

---
+++ js/game.js
@@ -0,0 +1,60 @@
+/**
+ *  
+ *  Author: Brady Murren
+ *  
+ */
+ 
+// mainState
+gameState.prototype = {
+    
+    //load all game assets
+    preload: function () {
+        game.load.image('player', 'resources/player.png');
+		game.load.image('ground', 'resources/ground.png');
+		game.load.image('player_projectile', 'resources/player_projectile.png');
+		game.load.image('boss', 'resources/boss.png');
+		game.load.image('boss_projectile', 'resources/boss_projectile.png');
+    },
+    
+    //constructor
+    create: function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		platformSetup();
+		
+		playerSetup();
+
+		initKeyboard();
+		
+		projectileSetup();
+		
+		bossSetup();
+		
+		basic = bossBasicMovement();
+		attack = slideAttack();
+	},
+
+    //game loop
+    update: function () {
+        game.physics.arcade.collide(player, ground);
+		game.physics.arcade.collide(player, boss1);
+		game.physics.arcade.overlap(player_projectiles, boss1, playerBulletHit, null, this);
+		game.physics.arcade.overlap(boss_projectiles, player, bossBulletHit, null, this);
+		
+		playerControls();
+		
+		bossSpin(false);
+		bossFire();
+		
+		attackManager();
+		
+		playerRegen();
+    },
+};
+
+//'gameDiv' is the id in index.html
+var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
+
+//links the name 'game' to the gameState
+game.state.add(states.game, gameState);
+game.state.start(states.game);
---
+++ js/player.js
@@ -0,0 +1,63 @@
+var player;
+var playerHealth = 300;
+var playerHealthText;
+var hitTimer = 0;
+var regenTimer = 0;
+
+function playerSetup() {
+	player = game.add.sprite(game.world.width / 2 - 32, game.world.height - 139,'player');
+	player.anchor.setTo(0.5,0.5);
+	game.physics.arcade.enable(player);
+	player.body.gravity.y = 800;
+	player.body.collideWorldBounds = true;
+	playerHealthText = game.add.text(600, 540, "Player Health " + playerHealth, style)
+}
+
+function playerControls() {
+	player.body.velocity.x = 0;
+
+		if (key_left.isDown)
+		{
+			player.body.velocity.x = -250;
+		}
+		else if (key_right.isDown)
+		{
+			player.body.velocity.x = 250;
+		}
+		if (key_jump.isDown && player.body.touching.down)
+		{
+			player.body.velocity.y = -600;
+		}
+		if (key_fire.isDown)
+		{
+			var bullet = player_projectiles.getFirstExists(false);
+			if (bullet) {
+                var length = player.width * 0.5;
+                var x = player.x + (Math.cos(player.rotation) * length);
+                var y = player.y + (Math.sin(player.rotation) * length);
+                
+                bullet.reset(x, y);
+				bullet.lifespan = 1500;
+                bullet.rotation = player.rotation;
+                
+                game.physics.arcade.velocityFromRotation(player.rotation, 500, bullet.body.velocity);
+            }
+		}
+		
+		player.rotation = game.physics.arcade.angleToPointer(player);
+}
+
+function playerRegen() {
+	if(hitTimer > 0){
+		hitTimer--;
+		regenTimer = 0;
+	}
+	else{
+		regenTimer++;
+		if (regenTimer >= 100) {
+			if(playerHealth < 300) playerHealth += 5;
+			playerHealthText.text = "Player Health " + playerHealth;
+			regenTimer = 0;
+		}
+	}
+}
